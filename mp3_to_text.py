71683224a8a613d767184bcc2b084332ca230318
cadupereira2109 <carlos.epereira@aluno.faculdadeimpacta.com.br>
2024-10-26 11:38:03 -0300

    Update mp3_to_text.py

--- mp3_to_text.py
+++ mp3_to_text.py
@@ -3,7 +3,7 @@ import assemblyai as aai
 aai.settings.api_key = "b8a3ef6ed06e4ec2a8cff577afaf3d30"
 transcriber = aai.Transcriber()
 
-mp3_filename = r'C:\Users\cadup\Desktop\automacao_de_atas\bcf824becd634f6dbe7200560eb7f10f.mp3'
+mp3_filename = r"C:\Users\cadup\Desktop\automacao_de_atas\bcf824becd634f6dbe7200560eb7f10f.mp3"
 
 config = aii.TranscriptionConfig(speaker_labels = True, speakers_expected = 2, langague_code = 'pt')
 
