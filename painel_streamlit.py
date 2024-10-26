decca0012a221b615188f36d61298aa72c60aeab
cadupereira2109 <carlos.epereira@aluno.faculdadeimpacta.com.br>
2024-10-26 12:10:01 -0300

    painel_streamlit.py

---
+++ painel_streamlit.py
@@ -0,0 +1,5 @@
+st.title('Automação de atas')
+
+st.write('Criando uma ferramenta de automação de atas de reunião com tecnologia de IA com Python')
+
+uploaded_file = st.file_uploader("Selecione o seu arquivo", accept_multiple_files=False, type = ['.mp4'])
