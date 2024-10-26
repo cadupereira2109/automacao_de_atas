ff918c2b7603f46d2b577f4dc3195eacb5e32834
cadupereira2109 <carlos.epereira@aluno.faculdadeimpacta.com.br>
2024-10-26 11:45:00 -0300

    Update mp3_to_text.py

--- mp3_to_text.py
+++ mp3_to_text.py
@@ -3,12 +3,14 @@ import assemblyai as aai
 aai.settings.api_key = "b8a3ef6ed06e4ec2a8cff577afaf3d30"
 transcriber = aai.Transcriber()
 
-mp3_filename = r"C:\Users\cadup\Desktop\automacao_de_atas\bcf824becd634f6dbe7200560eb7f10f.mp3"
+mp3_filename = r'C:\Users\cadup\Desktop\automacao_de_atas\bcf824becd634f6dbe7200560eb7f10f.mp3'
 
-config = aai.TranscriptionConfig(speaker_labels = True, speakers_expected = 2, language_code = 'pt')
+config = aai.TranscriptionConfig( speaker_labels = True, speakers_expected = 2, language_code = 'pt' )
 
-trasncriber = aai.Trascriber()
-trasncricao = trasncriber.transcribe(mp3_filename, config = config)
+transcriber = aai.Transcriber()
+transcricao = transcriber.transcribe(mp3_filename, config = config)
+
 
 for sentenca in transcricao.utterances:
-  print(f"Pessoa {sentenca.speaker}: {sentenca.text}")
+	print(f"Pessoa {sentenca.speaker}: {sentenca.text}")
+
